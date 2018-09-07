aa163c68facef9cb0bf0ccdb4ce1328b5b380f5c
petitlapin86 <skinnerpa86@yahoo.com>
2018-09-07 11:27:19 -0400

    rendering template index.pug

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-  res.send('<h1>I Love Flashcards!</h1>');
+  res.render('index');
 });
 
 app.get('/hello', (req, res) => {
