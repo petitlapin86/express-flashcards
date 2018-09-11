424b00d498ca9cf4b344ff6e56c95accefd4ccdf
petitlapin86 <skinnerpa86@yahoo.com>
2018-09-11 11:34:11 -0400

    create redirects and clear cookies

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ app.use(cookieParser());
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  res.render('index');
+  const name = req.cookies.username
+  if (name) {
+  res.render('index', { name });
+} else {
+  res.redirect('/hello');
+}
 });
 
 app.get('/cards', (req, res) => {
@@ -18,12 +23,22 @@ app.get('/cards', (req, res) => {
 });
 
 app.get('/hello', (req, res) => {
-  res.render('hello', { name: req.cookies.username});
+  const name = req.cookies.username;
+  if (name) {
+    res.redirect('/');
+  } else {
+    res.render('hello');
+  }
 });
 
 app.post('/hello', (req, res) => {
   res.cookie('username', req.body.username);
-  res.render('hello', {name: req.body.username });
+  res.redirect('/');
+});
+
+app.post('/goodbye', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/hello');
 });
 
 app.listen(3000, () => {
