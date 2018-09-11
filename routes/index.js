1489be1c889f1eddfa974ec7a9c1d452c11d7ba8
petitlapin86 <skinnerpa86@yahoo.com>
2018-09-11 15:22:04 -0400

    add data for flashcards and create route paramaters

--- routes/index.js
+++ routes/index.js
@@ -11,10 +11,6 @@ router.get('/', (req, res) => {
 }
 });
 
-router.get('/cards', (req, res) => {
-  res.render('card', { prompt: "Who is buried in Grant's tomb?", hint: "Think about whose tomb it is"});
-});
-
 router.get('/hello', (req, res) => {
   const name = req.cookies.username;
   if (name) {
